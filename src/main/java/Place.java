70840bc3f3dc57d7ac4a221921d96e412af8692a
herimihary <rherimiharyandoniaina@gmail.com>
2023-01-28 18:27:09 +0300

    init maping table

---
+++ src/main/java/Place.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author rheri
+ */
+public class Place {
+   public int  id;
+    public int numeroPlace;
+   public int  fk_idClasse;
+    
+    
+}
---
+++ src/main/java/Voyageur.java
@@ -0,0 +1,23 @@
+
+import java.util.Date;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author rheri
+ */
+public class Voyageur {
+   public int id;
+    public String  nom;
+    public String  preNom;
+     public Date datedeNaissance;
+    public String passeport;
+    
+    
+    
+    
+}
